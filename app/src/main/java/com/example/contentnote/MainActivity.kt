bdef85e0a6f9d5f7f91cbc3b7a99c184ebc5851f
KolaYAndr <nikolay_andreev02@mail.ru>
2023-07-22 17:03:21 +0300

    Commit 1

--- app/src/main/java/com/example/contentnote/MainActivity.kt
+++ app/src/main/java/com/example/contentnote/MainActivity.kt
@@ -4,43 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.contentnote.navigation.NotesNavHost
 import com.example.contentnote.ui.theme.ContentNoteTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ContentNoteTheme {
+                Scaffold(topBar = {
+                    TopAppBar(
+                        title = { Text(text = "C.Note") }
+                    )
+                },
+                    content = {
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(paddingValues = it),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            NotesNavHost()
+                        }
+                    }
+                )
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ContentNoteTheme {
-        Greeting("Android")
     }
 }
